d5189bda243c6df3afa0a8d53f7f6ed351d9ef9f
Chet Gray <chet.gray@waystar.com>
2022-11-30 18:19:41 -0500

    ♻ refactor: extract `setMessage` function

--- scripts/index.js
+++ scripts/index.js
@@ -18,6 +18,7 @@ form.elements["randomizeBtn"].addEventListener("click", function () {
 /**
  * Returns true if all the specified form fields are not-empty, and false otherwise. If
  * fields are not specified, all fields are checked.
+ *
  * @param {HTMLFormElement} form - The form element.
  * @param {?string[]} [fieldNames] - Names of fields to check. If `undefined` or `null`,
  * all fields will be checked.
@@ -47,38 +48,42 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   if (formIsComplete(form)) {
-    console.debug("submit event fired, setting success message");
     const empName = form.elements["empName"].value;
     const empId = form.elements["empId"].value;
     const empDateOfBirth = form.elements["empDateOfBirth"].value;
     const empDepartment = form.elements["empDepartment"].value;
     const empManager = form.elements["empManager"].value;
 
-    messageBlock.innerText = `New Employee Information for ${empName}, ID ${empId} accepted.`;
-    messageBlock.classList.add("message--success");
-    messageBlock.hidden = false;
-    const id = setTimeout(() => {
-      console.debug(`timeout ${id} fired, clearing success message`);
-      messageBlock.hidden = true;
-      messageBlock.innerText = "";
-      messageBlock.classList.remove("message--success");
-    }, 3000);
-    console.debug(`timeout ${id} set (success)`);
+    setMessage(
+      `New Employee Information for ${empName}, ID ${empId} accepted.`,
+      "success"
+    );
     form.reset();
     const newEmpLi = document.createElement("li");
     newEmpLi.innerText = `${empName} (${empId}), DOB: ${empDateOfBirth}, Dep: ${empDepartment}, Mgr: ${empManager}`;
     list.appendChild(newEmpLi);
   } else {
-    console.debug("submit event fired, setting error message");
-    messageBlock.innerText = "Please complete all fields.";
-    messageBlock.classList.add("message--error");
-    messageBlock.hidden = false;
-    const id = setTimeout(function () {
-      console.debug(`timeout ${id} fired, clearing error message`);
-      messageBlock.hidden = true;
-      messageBlock.innerText = "";
-      messageBlock.classList.remove("message--error");
-    }, 3000);
-    console.debug(`timeout ${id} set (error)`);
+    setMessage("Please fill out all fields.", "error");
   }
 });
+
+/**
+ * Sets the message block to display a message, and clears it after a timeout.
+ *
+ * @param {string} message - The message to display.
+ * @param {string} [type="info"] - The type of message. (Default: "info")
+ * @param {number} [timeout=3000] - The number of milliseconds to display the message. (Default: 3000)
+ */
+function setMessage(message, type = "info", timeout = 3000) {
+  console.debug(`setting ${type} message`);
+  messageBlock.innerText = message;
+  messageBlock.classList.add("message--" + type);
+  messageBlock.hidden = false;
+  const timeoutId = setTimeout(function () {
+    console.debug(`timeout ${timeoutId} fired, clearing ${type} message`);
+    messageBlock.hidden = true;
+    messageBlock.innerText = "";
+    messageBlock.classList.remove("message--" + type);
+  }, timeout);
+  console.debug(`timeout ${timeoutId} set for ${type} message`);
+}
