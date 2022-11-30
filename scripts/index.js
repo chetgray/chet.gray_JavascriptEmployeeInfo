d9e8d4e8cae9258bac3451fbee8c7614c10c5f9f
Chet Gray <chet.gray@waystar.com>
2022-11-30 15:02:13 -0500

    ✨ feat: create predicate to check form completion

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+/**
+ * @file scripts/index.js - Handles the new employee form and employees list.
+ * @author Chet Gray <chet.gray@waystar.com>
+ */
+
+/**
+ *
+ * Returns true if all the specified form fields are not-empty, and false otherwise. If
+ * fields are not specified, all fields are checked.
+ *
+ * @param {HTMLFormElement} form - The form element.
+ * @param {?string[]} [fieldNames] - Names of fields to check. If `undefined` or `null`,
+ * all fields will be checked.
+ * @returns {boolean} True if all the checked fields are not-empty, and false otherwise.
+ */
+function formIsComplete(form, fieldNames) {
+  if (fieldNames === undefined || fieldNames === null) {
+    // `HTMLFormControlsCollection` is an array-like object, so we can call the
+    // `Array.prototype.every()` method with it.
+    // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every#calling_every_on_non-array_objects)
+    // I would prefer to shallow-copy it into an array with `Array.from()`, but that is
+    // not supported in IE. (https://caniuse.com/mdn-javascript_builtins_array_from)
+    const isComplete = Array.prototype.every.call(
+      form.elements,
+      (field) => field.value !== ""
+    );
+    return isComplete;
+  } else {
+    const isComplete = fieldNames.every(
+      (fieldName) => form.elements[fieldName].value !== ""
+    );
+    return isComplete;
+  }
+}
